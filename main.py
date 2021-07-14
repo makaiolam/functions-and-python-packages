5919916d5e5d92ca00a815c0bff211ac89cedd42
Makaiolam <87239912+makaiolam@users.noreply.github.com>
2021-07-14 22:41:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,78 @@
+# number = input("type a player number")
+# number = int(number)
+# if number > 0:
+#   print("number is greater than 0 (positive)")
+# elif number < 0:
+#   print("number less than 0 (negative)")
+# else:
+#   print("number is 0")
+
+
+# new lines: "/n" , print()
+
+# for i in range(1,7):
+#   for j in range(0,i):
+#     print(j + 1, end=" ")
+#   print()
+
+# funcions
+#------------------------
+# math definition: inputs provides outputs
+# programming definition: functions take in inputs (parameters) and returns value
+
+# why we use funtions
+# break up into managble chuncks
+# ability to reuse code rather than copying and pasting it
+# makes more orginized code
+
+# no value returned == void function 
+# def helloworld():
+#   print("hello world")
+
+#called a function
+# helloworld()
+
+
+# with parameters
+# def greeting(name):
+#   print("hello" + name)   
+
+# myname = input("what is your name")
+# greeting(myname)
+
+#return fuction
+# def addnumbers(inputnumbers, inputnumber2):
+#   output = int(inputnumber1) + int(inputnumber2)
+#   return output
+
+# inputnumber1 = input("enter first number")
+# inputnumber2 = input("enter second number")
+
+# print(addnumbers(inputnumber1, inputnumber2))
+
+# def multipynumber(inputnumber1, inputnumber2):
+#   output = int(inputnumber1) * int(inputnumber2)
+#   return output
+
+# inputnumber1 = input("enter first number")
+# inputnumber2 = input("enter second number")
+
+# print(multipynumber (inputnumber1, inputnumber2))
+
+
+# python packages
+# python comes with libaries. libaries/packages in python contain useful functions that we can use to make writing code easier for us.
+# ex. import random
+# ex. import turtle
+
+import turtle
+W = turtle.Turtle()
+W.penup()
+W.setty(100)
+W.pendown()
+
+
+
+
+
+
